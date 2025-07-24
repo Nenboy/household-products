2936e246d8bb435eef03c3d53c3acc87b858e7b3
Paul Gambo <paulgambo894@gmail.com>
2025-07-24 12:34:19 +0100

    WIP: Saving current changes before checkout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from 'react'
+<<<<<<< Updated upstream
 import Profile from './pages/Profile';
+=======
+import { Routes, Route } from 'react-router-dom'
+import Profile from './pages/Profile/Profile';
+>>>>>>> Stashed changes
 import './App.css'
 
 function App() {
   return (
     <div>
+<<<<<<< Updated upstream
+=======
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+      </Routes>
+>>>>>>> Stashed changes
       <Profile/>
     </div>
   )
