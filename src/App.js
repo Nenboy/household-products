9fa09e451b11ed9a849a71517b227f3a0da246b2
Nenboy <dechinengak10@gmail.com>
2025-07-24 14:52:50 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 
-import React from 'react'
-
-// import Profile from './pages/Profile';
-import Profile from './pages/Profile';
-
-import { Routes, Route } from 'react-router-dom'
-import Profile from './pages/Profile/Profile';
-import './App.css'
-import Homepage from './pages/Home/Home'
+import './App.css';
+import Homepage from './pages/Home/Home';
+import Profile from './pages/Profile/Profile'; // Use only one import for Profile
 
 function App() {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Homepage />} />
-        <Route path="/profile" element={<Profile />}
+        <Route path="/profile" element={<Profile />} /> {/* ✅ properly closed tag */}
       </Routes>
     </div>
-  )
+  );
 }
 
-export default App
-
+export default App;
